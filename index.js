21dc2247fe34da25e8d9566bf74b815481724075
Zemmouri Farouk <faroukwebdesign@gmail.com>
2023-08-18 04:51:37 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const deleteMain = document.getElementById('main');
+deleteMain.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
+
+
+
+
